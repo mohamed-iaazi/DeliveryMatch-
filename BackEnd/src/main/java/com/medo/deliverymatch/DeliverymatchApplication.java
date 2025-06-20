d78b9f2726e92dfba6e8928501fb2b95d6f4e880
mohamed <iaazi.mohamed.1998@gmail.com>
2025-06-20 09:57:20 +0100

    create the spring boot project for backend

---
+++ BackEnd/src/main/java/com/medo/deliverymatch/DeliverymatchApplication.java
@@ -0,0 +1,13 @@
+package com.medo.deliverymatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverymatchApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/medo/deliverymatch/DeliverymatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medo.deliverymatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverymatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
