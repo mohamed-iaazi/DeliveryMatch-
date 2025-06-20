20ca53d6057c0b3449131dc35c6f62c9560fc46e
mohamed <iaazi.mohamed.1998@gmail.com>
2025-06-20 12:07:01 +0100

    cree colis entity

--- BackEnd/src/main/java/com/medo/deliverymatch/model/Colis.java
+++ BackEnd/src/main/java/com/medo/deliverymatch/model/Colis.java
@@ -1,4 +1,26 @@
 package com.medo.deliverymatch.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Colis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+    private String dimension;
+    private String typeColis;
+    private double poids;
 }
